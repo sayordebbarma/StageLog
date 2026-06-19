9114c7188a3ca56381ef8b06445d6aeed7277107
sayordebbarma <sayorofficial8543@gmail.com>
2026-06-19 22:15:37 +0530

    Initial Commit

---
+++ StageLog/StageLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  StageLogApp.swift
+//  StageLog
+//
+//  Created by Sayor Debbarma on 19/06/26.
+//
+
+import SwiftUI
+
+@main
+struct StageLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
